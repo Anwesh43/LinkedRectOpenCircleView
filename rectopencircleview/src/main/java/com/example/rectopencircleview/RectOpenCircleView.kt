4212b58ad7ef7bb9f3a2f7d362aa61d6b92f1234
Anwesh43 <anweshthecool0@gmail.com>
2021-03-26 00:35:47 +0530

    adding Canvas extension methods

--- rectopencircleview/src/main/java/com/example/rectopencircleview/RectOpenCircleView.kt
+++ rectopencircleview/src/main/java/com/example/rectopencircleview/RectOpenCircleView.kt
@@ -28,3 +28,28 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawRectOpenCircle(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / 2
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        drawRect(RectF(-w / 2, -h / 2, -w / 2 * sf2, -h / 2 + h * sf1), paint)
+        restore()
+    }
+    drawArc(RectF(-size, -size, size, size), 0f, deg * sf3, true, paint)
+    restore()
+}
+
+fun Canvas.drawROCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawRectOpenCircle(scale, w, h, paint)
+}
