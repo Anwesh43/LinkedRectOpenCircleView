9cf1e081ba25a1a4416f04f0caae421e6c5ec5e9
Anwesh43 <anweshthecool0@gmail.com>
2021-03-26 00:36:50 +0530

    adding RectOpenCircleView class

--- rectopencircleview/src/main/java/com/example/rectopencircleview/RectOpenCircleView.kt
+++ rectopencircleview/src/main/java/com/example/rectopencircleview/RectOpenCircleView.kt
@@ -53,3 +53,19 @@ fun Canvas.drawROCNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawRectOpenCircle(scale, w, h, paint)
 }
+
+class RectOpenCircleView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
