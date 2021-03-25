e3b2492bcbd8ee28b53bdcd9312758120c3477ee
Anwesh43 <anweshthecool0@gmail.com>
2021-03-26 00:51:32 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/example/linkedrectopencircleview/MainActivity.kt
+++ app/src/main/java/com/example/linkedrectopencircleview/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.linkedrectopencircleview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.rectopencircleview.RectOpenCircleView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RectOpenCircleView.create(this)
+        fullScreen()
     }
+}
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
